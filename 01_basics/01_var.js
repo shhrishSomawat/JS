5c25d34cd7a727a977264f6ff667c4bb96a4daa0
Shrish Somawat <120320230+shhrishSomawat@users.noreply.github.com>
2024-01-23 08:14:26 +0000

    datatypes

---
+++ 01_basics/01_var.js
@@ -0,0 +1,14 @@
+const accoundId = 144553
+let accountEmail = "shrishsomawat982@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accoundId = 2 //Not allowed
+accountEmail = "ss@ss.com"
+accountPassword = "67890"
+accountCity = "kota"
+
+console.log(accoundId);
+// console.log(accountEmail);
+console.table([accoundId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hElLo");  //we are using node js   not browser
+
+let name = "shrish"; //strings
+let age = 22; //integer  => 2^53
+let isLoggedIn = true; //booleaens
+
+
+
+//***********************// all above datatypes are primitve data types....... */
+//Null -->standlone value  yeh representation hai empty value hai intentionally bata raha hu ki khali hai ! replaeced with 0
+//Undefined // jab value define nahi kari hai undefined hai variable declared h par value nahi hai
+//symbol?--> uniqueness refers to this
+
+
+//Object
+console.log(typeof("shrish"))
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
