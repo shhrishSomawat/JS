34bfe6574919b288ab496570d29f8756f93a7ef4
Shrish Somawat <120320230+shhrishSomawat@users.noreply.github.com>
2024-01-23 10:06:29 +0000

    date time commited

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,28 @@
+// //Dates
+// let myDate = new Date();
+// // console.log(myDate);
+// //converting into string
+// // console.log(myDate.toString());
+// // console.log(myDate.toDateString());
+// // console.log(myDate.toISOString());
+// // console.log(myDate.toJSON());
+// // console.log(myDate.toLocaleDateString());
+// // console.log(myDate.toLocaleTimeString());
+
+// // let myTimeStamp = Date.now()
+// // console.log(myTimeStamp); //milliseconds
+// // i want to convert mytinestamo from ms to s
+// // console.log(Math.floor(myTimeStamp/1000)); //divide to thoudands
+
+// let newDate = new Date();
+// // console.log(newDate);
+// // console.log(newDate.getMonth()+1);
+// // console.log(newDate.getDay());
+// // console.log(newDate.getFullYear());
+
+// console.log(
+//   newDate.toLocaleString("default", {
+//     // console.log(newDate.getMonth());
+//     weekday: "long",
+//   })
+// );
