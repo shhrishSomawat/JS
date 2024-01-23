9bca219c5652077aff18634f3fdd6ea60ed689a7
Shrish Somawat <120320230+shhrishSomawat@users.noreply.github.com>
2024-01-23 09:06:42 +0000

    upto Comparisions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,44 @@
+let score = "33";
+//console.log(typeof score);  //it will give string
+//for converting string to number in  js
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); // now here the type is number not a string
+
+/*
+"33" => 33;
+"33abc" =>NaN;
+true => 1;
+false => 0;
+*/
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);  //it gives false
+
+let someNumbers = 47;
+let stringNumber = String(someNumbers);
+// console.log(typeof stringNumber);
+
+//**********************************************************OPERATIONS********************************************************************* */
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "hello";
+let str2 = " Hitesh";
+
+let str = str1 + str2;
+// console.log(str);
+
+// console.log("1" + 2);
+// // console.log(1 + "2");
+// console.log("1"+2+2+2+2); //12222
+// console.log(1+2+"2"+"2"); //322
---
+++ 01_basics/04_Comparisons.js
@@ -0,0 +1,3 @@
+console.log(2>1);
+console.log("2">1) //true?
+// compariison s
