256f878b8a9a580f255cc036c94c90b9305f27df
Shrish Somawat <120320230+shhrishSomawat@users.noreply.github.com>
2024-01-23 09:41:50 +0000

    numbers and maths

---
+++ 01_basics/06_Nums&Maths.js
@@ -0,0 +1,36 @@
+const score = 1000
+const score1 = new Number(1000.9090).toFixed(2);
+// console.log(score);
+// console.log(score1);
+// console.log(score1);
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++ - MATHS - +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-3)) // it gives +3
+// console.log(Math.round(4.7)); //5
+// console.log(Math.ceil(3.1)); //upper value
+// console.log(Math.floor(3.9999)); //lower value that is 3
+// console.log(Math.sqrt(4)); //sqrt of 4  = 2
+// console.log(Math.pow(3,2)); //3 raised to power 2
+// console.log(Math.min(1,2,3,4,5,6,0)); //min value
+// console.log(Math.max(0,333333,23,0.9));//max value
+// console.log(Math.random()); // always come between 0 -1
+// console.log(Math.random()*10); // it gives 0-9 inclusively
+// console.log(Math.random()*10+1) // in case of value become 0 it adds 1 so that 0 never come 
+
+
+min=10
+max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
